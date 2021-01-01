94556fbad0ae042be0db5ef7253099a814ca7d26
Joyson P S <joysonpathra@gmail.com>
2021-01-01 23:30:47 +0530

    Bug fixes

--- byjoy.js
+++ byjoy.js
@@ -1,5 +1,5 @@
 const text = document.querySelectorAll("#byjoy path");
-console.log("Text", text);
+// console.log("Text", text);
 for (let i = 0; i < text.length; i++) {
-  console.log(text[i].getTotalLength());
+  //   console.log(text[i].getTotalLength());
 }
